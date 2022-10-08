20a6d4c1742c38bb4a96d79d238548b321441fff
Timothé <timo.2001@hotmail.fr>
2022-10-09 01:22:14 +0200

    Connection et récupération du calendrier

--- go.mod
+++ go.mod
@@ -2,4 +2,17 @@ module LDV_RollcallBot
 
 go 1.19
 
-require github.com/joho/godotenv v1.4.0 // indirect
+require (
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/apognu/gocal v0.9.0
+	github.com/joho/godotenv v1.4.0
+	golang.org/x/net v0.0.0-20221004154528-8021a29435af
+)
+
+require (
+	github.com/ChannelMeter/iso8601duration v0.0.0-20150204201828-8da3af7a2a61 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/procyon-projects/chrono v1.1.2 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,117 @@
 package main
 
 import (
+	"github.com/PuerkitoBio/goquery"
+	"github.com/apognu/gocal"
 	"github.com/joho/godotenv"
-	_ "github.com/joho/godotenv/autoload"
+	"io"
 	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"os"
+	"time"
 )
 
 var login string
 var password string
+var baseURL = "https://www.leonard-de-vinci.net"
+var client http.Client
 
 func main() {
+	//taskScheduler := chrono.NewDefaultTaskScheduler()
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	login := os.Getenv("S3_BUCKET")
-	password := os.Getenv("SECRET_KEY")
+	login = os.Getenv("LOGIN")
+	password = os.Getenv("PASSWORD")
+
+	jar, err := cookiejar.New(nil)
+
+	client = http.Client{
+		Jar: jar,
+	}
+
+	authCookies()
+	getCalendar()
+}
+
+func logError(err error) {
+	if err != nil {
+		print("[ERROR] ")
+		log.Fatal(err)
+	}
+}
+
+func authCookies() {
+	samlLink := func() string {
+		//Get SAML Link from ajax
+		resp, err := client.PostForm(baseURL+"/ajax.inc.php", url.Values{"act": {"ident_analyse"}, "login": {login}})
+		logError(err)
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		logError(err)
+		//Trim the link from the response
+		samlLink := string(body[25 : len(body)-3])
+		return samlLink
+	}
+	adfsURL := func() string {
+		//Get ADFS URL from SAML Link
+		resp, err := client.Get(baseURL + samlLink())
+		logError(err)
+		defer resp.Body.Close()
+		//Get the URL
+		adfsURL := resp.Request.URL.String()
+		return adfsURL
+	}
+	samlResponse := func() string {
+		//Get the cookies from ADFS by posting the login and password
+		resp, err := client.PostForm(adfsURL(), url.Values{"UserName": {login}, "Password": {password}, "AuthMethod": {"FormsAuthentication"}})
+		logError(err)
+		defer resp.Body.Close()
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		logError(err)
+		//Get the SAMLResponse from the form
+		samlResponse, _ := doc.Find("input[name=SAMLResponse]").Attr("value")
+		return samlResponse
+	}
+	//Post the SAMLResponse to simpleSAML
+	resp, err := client.PostForm(baseURL+"/simplesaml/module.php/saml/sp/saml2-acs.php/alv-sp", url.Values{"SAMLResponse": {samlResponse()}, "RelayState": {samlLink()}})
+	logError(err)
+	defer resp.Body.Close()
+	//Go to the saml link to get the cookies
+	resp, err = client.Get(baseURL + samlLink())
+	logError(err)
+	defer resp.Body.Close()
+	//print the body to see if it worked
+	_, err = io.ReadAll(resp.Body)
+	logError(err)
+	println("[INFO] Authenticated")
+}
+
+func getCalendar() {
+	calLink := func() string {
+		resp, err := client.Get(baseURL)
+		logError(err)
+		defer resp.Body.Close()
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		logError(err)
+		return doc.Find("#main > div:nth-child(3) > div > div > div > header > div > a").AttrOr("href", "error")
+	}
+	calendar := func() []gocal.Event {
+		ical, err := client.Get(calLink())
+		logError(err)
+		defer ical.Body.Close()
+		//Parse the ical
+		start, end := time.Now(), time.Now().Add(7*24*time.Hour)
+		c := gocal.NewParser(ical.Body)
+		c.Start, c.End = &start, &end
+		c.Parse()
+		return c.Events
+	}
+	for _, event := range calendar() {
+		println(event.Summary + " de " + event.Start.Format("15:04") + " à " + event.End.Format("15:04"))
+	}
 }
